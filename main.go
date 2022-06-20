597c9c504548a0542e9c3f5849fc44553a9ee74c
izaakdale <64863677+izaakdale@users.noreply.github.com>
2022-06-20 03:27:06 +0100

    Added hackerrank minmaxsum, timeconversion, findlonelyint, diagonaldifference solutions

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 	"strings"
 	"unicode"
 )
@@ -17,6 +19,100 @@ func main() {
 
 	arr := []int{-4, 3, -9, 0, 4, 1}
 	countArrayNumbers(arr)
+
+	arr2 := []int32{1, 2, 3, 4, 5}
+	miniMaxSum(arr2)
+
+	timeConversion("05:01:00PM")
+	timeConversion("05:01:00AM")
+
+	findLonelyInt([]int{1, 2, 3, 4, 3, 2, 1})
+
+	diagonalDifference([][]int{
+		{11, 2, 4},
+		{4, 5, 6},
+		{10, 8, -12},
+	})
+
+}
+
+func diagonalDifference(arr [][]int) {
+	lrSum, rlSum := 0, 0
+
+	for i := 0; i < len(arr); i++ {
+		lrSum += arr[i][i]
+		rlSum += arr[i][len(arr)-i-1]
+	}
+
+	var diff int
+	if lrSum > rlSum {
+		diff = lrSum - rlSum
+	} else {
+		diff = rlSum - lrSum
+	}
+
+	fmt.Println(diff)
+}
+
+func findLonelyInt(arr []int) {
+	var lonelyInt int
+
+	intFinder := make(map[int]bool)
+	for _, v := range arr {
+		_, found := intFinder[v]
+		if !found {
+			lonelyInt = v
+			intFinder[v] = false
+		} else {
+			intFinder[v] = true
+		}
+	}
+	fmt.Println(lonelyInt)
+}
+
+func timeConversion(s string) {
+	var hh, mm, ss int
+	var err error
+
+	hh, err = strconv.Atoi(s[0:2])
+	mm, err = strconv.Atoi(s[3:5])
+	ss, err = strconv.Atoi(s[6:8])
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if hh == 12 {
+		if s[8:] == "AM" {
+			hh = 0
+		}
+	} else {
+		if s[8:] == "PM" {
+			hh += 12
+		}
+	}
+
+	fmt.Printf("%.2d:%.2d:%.2d\n", hh, mm, ss)
+}
+
+func miniMaxSum(arr []int32) {
+	var sum, min, max int32
+
+	for _, v := range arr {
+		sum += v
+	}
+	// in order to compare min needs to be a high value
+	max = 0
+	min = sum
+	for _, v := range arr {
+		if sum-v < min {
+			min = sum - v
+		}
+		if sum-v > max {
+			max = sum - v
+		}
+	}
+
+	fmt.Printf("%d %d\n", min, max)
 }
 
 func countArrayNumbers(arr []int) {
